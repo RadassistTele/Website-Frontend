b53cf7a615155da56033f4a23f88c47d7f56eb6c
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2025-08-08 01:26:24 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      this is the page component
+      <p>Welcome to the page!</p>
+    </div>
+  )
+}
+
+export default page
